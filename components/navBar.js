ee1d66169646dc0f2267f9104c8acb557c182622
sydney-rd <sydneydavid56@gmail.com>
2026-04-19 19:21:38 -0400

    removing res

--- components/navBar.js
+++ components/navBar.js
@@ -1,94 +1,94 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import { Box, VStack, Image } from '@chakra-ui/react'
-import Link from 'next/link'
-import Logo from '../public/assets/logo/logo.png'
+// import React from 'react'
+// import { useRouter } from 'next/router'
+// import { Box, VStack, Image } from '@chakra-ui/react'
+// import Link from 'next/link'
+// import Logo from '../public/assets/logo/logo.png'
 
-export default function Navbar() {
-  const router = useRouter()
+// export default function Navbar() {
+//   const router = useRouter()
 
-  const navBarStyle = {
-    color: 'white',
-    whiteSpace: 'pre',
-    WebkitTextStroke: '1px #4B0082',
-    transition: 'color 0.3s',
-    fontSize: '.7rem',
-    fontWeight: 'bold',
-    fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
-    filter: 'brightness(150%)',
-    transitionDelay: '0.8s',
-    userSelect: 'none',
-    cursor: 'crosshair'
-  }
-  const activeLinkStyle = {
-    ...navBarStyle,
-    borderBottom: '.6px solid white'
-  }
+//   const navBarStyle = {
+//     color: 'white',
+//     whiteSpace: 'pre',
+//     WebkitTextStroke: '1px #4B0082',
+//     transition: 'color 0.3s',
+//     fontSize: '.7rem',
+//     fontWeight: 'bold',
+//     fontFamily: 'Verdana, Geneva, Tahoma, sans-serif',
+//     filter: 'brightness(150%)',
+//     transitionDelay: '0.8s',
+//     userSelect: 'none',
+//     cursor: 'crosshair'
+//   }
+//   const activeLinkStyle = {
+//     ...navBarStyle,
+//     borderBottom: '.6px solid white'
+//   }
 
-  const isLinkActive = href => router.pathname === href
+//   const isLinkActive = href => router.pathname === href
 
-  return (
-    <Box
-      position="fixed"
-      top="2rem"
-      right="0"
-      h="50vh"
-      display="flex"
-      flexDirection="column"
-      justifyContent="flex-start"
-      alignItems="flex-end"
-      paddingRight="4"
-      zIndex="1"
-    >
-      <Link href="/" passHref>
-        <Image
-          src={Logo.src}
-          alt="Logo"
-          w="90px"
-          style={{ cursor: 'crosshair' }}
-        />
-      </Link>
-      <VStack spacing={20} align="flex-end">
-        <Box pr=".6rem" pt="2">
-          <Box transform="rotate(90deg)" whiteSpace="pre">
-            <Link
-              href="/about"
-              style={isLinkActive('/about') ? activeLinkStyle : navBarStyle}
-            >
-              ABOUT
-            </Link>
-          </Box>
-        </Box>
-        <Box pt="2" pr="2">
-          <Box transform="rotate(90deg)" whiteSpace="pre">
-            <Link
-              href="/projectPage"
-              style={
-                isLinkActive('/projectPage') ? activeLinkStyle : navBarStyle
-              }
-            >
-              EVENTS
-            </Link>
-          </Box>
-        </Box>
-        <Box pr="2" pt="4">
-          <Box
-            className="navBar"
-            transform="rotate(90deg)"
-            whiteSpace="pre"
-            style={navBarStyle}
-          >
-            {/* <a
-              href="/assets/Sydney-David-Events-Resume.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={navBarStyle}
-            >
-              RESUME
-            </a> */}
-          </Box>
-        </Box>
-      </VStack>
-    </Box>
-  )
-}
+//   return (
+//     <Box
+//       position="fixed"
+//       top="2rem"
+//       right="0"
+//       h="50vh"
+//       display="flex"
+//       flexDirection="column"
+//       justifyContent="flex-start"
+//       alignItems="flex-end"
+//       paddingRight="4"
+//       zIndex="1"
+//     >
+//       <Link href="/" passHref>
+//         <Image
+//           src={Logo.src}
+//           alt="Logo"
+//           w="90px"
+//           style={{ cursor: 'crosshair' }}
+//         />
+//       </Link>
+//       <VStack spacing={20} align="flex-end">
+//         <Box pr=".6rem" pt="2">
+//           <Box transform="rotate(90deg)" whiteSpace="pre">
+//             <Link
+//               href="/about"
+//               style={isLinkActive('/about') ? activeLinkStyle : navBarStyle}
+//             >
+//               ABOUT
+//             </Link>
+//           </Box>
+//         </Box>
+//         <Box pt="2" pr="2">
+//           <Box transform="rotate(90deg)" whiteSpace="pre">
+//             <Link
+//               href="/projectPage"
+//               style={
+//                 isLinkActive('/projectPage') ? activeLinkStyle : navBarStyle
+//               }
+//             >
+//               EVENTS
+//             </Link>
+//           </Box>
+//         </Box>
+//         <Box pr="2" pt="4">
+//           <Box
+//             className="navBar"
+//             transform="rotate(90deg)"
+//             whiteSpace="pre"
+//             style={navBarStyle}
+//           >
+//             {/* <a
+//               href="/assets/Sydney-David-Events-Resume.pdf"
+//               target="_blank"
+//               rel="noopener noreferrer"
+//               style={navBarStyle}
+//             >
+//               RESUME
+//             </a> */}
+//           </Box>
+//         </Box>
+//       </VStack>
+//     </Box>
+//   )
+// }
